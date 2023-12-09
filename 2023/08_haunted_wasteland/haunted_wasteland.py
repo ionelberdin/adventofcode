6fb658388133bb98d4e66d420e7f511cd7221940
Ion <ionelberdin@gmail.com>
2023-12-09 01:38:37 +0100

    Problem 2 was tough, but it's solved now as well.

--- 2023/08_haunted_wasteland/haunted_wasteland.py
+++ 2023/08_haunted_wasteland/haunted_wasteland.py
@@ -4,15 +4,17 @@
 
 Status:
     - Part 1:
-        * Test passing
-        * Puzzle passing
+        * Test 1: PASS
+        * Test 2: PASS
+        * Puzzle: PASS
     - Part 2:
-        * Tests passing
-        * Puzzle fails --> solution doesn't converge
+        * Test 3: PASS 
+        * Puzzle: PASS
 '''
 import re
-from argparse import ArgumentParser
+from functools import reduce
 from itertools import cycle
+from math import gcd
 
 LINE_PATTERN = re.compile('(\w+) \= \((\w+), (\w+)\)')
 
@@ -32,24 +34,37 @@ def parse_input(filepath:str):
 def get_network(lines:list):
     return {pos: {'L': left, 'R': right} for pos, left, right in lines}
 
-# 1. Functions that are specific for problem 1
-
-def haunted_wasteland(filepath:str):
-    directions, lines = parse_input(filepath)
-    network = get_network(lines)
-
-    goal = re.compile('ZZZ')
-    return navigate(network, directions, position='AAA', goal=goal)
-
 def navigate(network:dict, directions:str, position:str, goal:re.Pattern):
     for n, direction in enumerate(cycle(directions), 1):
         position = network[position][direction]
         if (goal.match(position)):
             return n
 
+# 1. Functions that are specific for problem 1
+
+def haunted_wasteland_1(filepath:str):
+    directions, lines = parse_input(filepath)
+    network = get_network(lines)
+
+    goal = re.compile('ZZZ')
+    return navigate(network, directions, position='AAA', goal=goal)
+
 # 2. Functions that are specific for problem 2
 
-def haunted_wasteland_01(filepath:str):
+'''Problem 2 doesn't have a straight forward solution, because it 
+requires you to dig into the data, otherwise the solution won't converge.
+When you look into the data and you realise that each ghost only arrives
+to a certain goal and does't pass through the others, and once it passes, 
+it does so AGAIN and AGAIN in the same number of steps over and over.
+Furthermore, all ghosts arrive to their goals in a multiple of the 
+number of directions provided by the puzzle input (307 in my case).
+Hence it's only necessary to calculate in how many steps each ghost
+arrives for the 1st time to its goal and then calculate the least
+common multiple of all of them, which is crazy high --> 10921547990923.
+Once you realise this, it takes no time to find the solution.
+'''
+
+def haunted_wasteland_2(filepath:str):
     directions, lines = parse_input(filepath)
     network = get_network(lines)
 
@@ -58,20 +73,15 @@ def haunted_wasteland_01(filepath:str):
     goal = re.compile('\w{2}Z')
     positions = list(filter(lambda x: INIT_POS.match(x), network))
 
-    return navigate_all(network, directions, positions, goal)
+    steps = map(lambda x: navigate(network, directions, x, goal), positions)
 
-def navigate_all(network:dict, directions:list, positions:list, goal:re.Pattern):
-    for n, direction in enumerate(cycle(directions), 1):
-        positions = [network[position][direction] for position in positions]
-        if (any([goal.match(position) for position in positions])):
-            print(n, positions, [goal.match(position) for position in positions])
-        if (all([goal.match(position) for position in positions])):
-            return n
+    return reduce(lambda x, y: int((x * y) / gcd(x, y)), steps)
 
 if __name__ == '__main__':
-    VERSIONS = {0: haunted_wasteland, 1: haunted_wasteland_01}
-    parser = ArgumentParser()
-    parser.add_argument('filepath')
-    parser.add_argument('-v', '--version', type=int, default=0)
-    args = parser.parse_args()
-    print(VERSIONS[args.version](args.filepath))
+    assert(haunted_wasteland_1('test_01.txt') == 2)
+    assert(haunted_wasteland_1('test_02.txt') == 6)
+    assert(haunted_wasteland_1('puzzle_input.txt') == 14429)
+    assert(haunted_wasteland_2('test_03.txt') == 6)
+    assert(haunted_wasteland_2('puzzle_input.txt') == 10921547990923)
+
+    print(haunted_wasteland_2('puzzle_input.txt'))
