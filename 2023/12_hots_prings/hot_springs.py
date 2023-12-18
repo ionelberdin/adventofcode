47184965abdc75892f03c3641fbbd5e9a54a91ba
Ion <ionelberdin@gmail.com>
2023-12-18 20:50:15 +0100

    Continued working on a solution for the 1st part, still not passing.

--- 2023/12_hots_prings/hot_springs.py
+++ 2023/12_hots_prings/hot_springs.py
@@ -13,7 +13,7 @@ Status:
 
 import re
 
-from itertools import permutations, product
+from itertools import pairwise, permutations, product
 
 # 0. Functions that are common to both problems
 
@@ -49,10 +49,11 @@ def get_arrangements(record:str):
     return len([x for x in arrangements])
 
 def get_arrangements_2(record):
-    record, damaged_group_sizes = record.split(' ')
-    damaged_group_sizes = [int(x) for x in damaged_group_sizes.split(',')]
+    print(record)
+    record, damaged_groups = record.split(' ')
+    damaged_groups = [int(x) for x in damaged_groups.split(',')]
 
-    total_damaged = sum(damaged_group_sizes)
+    total_damaged = sum(damaged_groups)
     total_unknowns = len(UNKNOWN.findall(record))
     total_damaged_in_str = len(DAMAGED.findall(record))
 
@@ -61,14 +62,39 @@ def get_arrangements_2(record):
 
     total_operational = len(record) - total_damaged
     
-    return get_operational_cases(total_operational, len(damaged_group_sizes))
+    operational_cases = get_operational_cases(total_operational, len(damaged_groups))
+    operational_cases = map(lambda x: build_record(x, damaged_groups), operational_cases)
+    operational_cases = filter(pattern.match, operational_cases)
+    return len(list(map(lambda x: 1, operational_cases)))
 
 def get_operational_cases(operational:int, damaged_groups:int):
-    internal = list([tuple(range(1, operational - damaged_groups + 2))]) * (damaged_groups - 1)
-    external = [tuple(range(operational - damaged_groups + 1))]
-    cases = product(*[external + internal + external])
-    return len([x for x in cases])
+    '''Given the number of operational hot springs, and the amount of damaged groups, 
+    the strategy is to consider all the possible cases of groups of operational
+    hot springs in between the non-operational ones.
+    This should reduce the number of cases to check in comparison with the strategy
+    followed in the first attempt.
+    We may divide the types of operational hot spring groups into those that are
+    external to the non-operational ones, and the internal ones.
+    The difference between both of those types is:
+        * The external belong to the integer group bewteen 0 and operational - (damaged_groups - 1)
+            + There are only 2 possible external groups.
+        * The internal belong to the integer group between 1 and operational - (damaged_groups - 1) + 1
+            + There are always N-1 internal groups, being N the number of damaged_groups.
+    all boundaries included.
+    eg: Ext1 + DG1 + Int1 + DG2 + Int2 + DG3 + Ext2
+    '''
+    internal_groups = damaged_groups - 1
+    external = [tuple(range(operational - internal_groups + 1))]
+    internal = list([tuple(range(1, operational - internal_groups + 2))]) * (damaged_groups - 1)
+    return map(lambda x: x[0], product(*[external + internal + external]))
 
+def build_record(operational_groups, damaged_groups):
+    record = '.' * operational_groups[0]
+    for damaged, operational in zip(damaged_groups, operational_groups[1:]):
+        print(damaged, operational)
+        record += '#' * damaged + '.' * operational
+    print(operational_groups, damaged_groups, record)
+    return record
 
 def hot_springs(filepath:str):
     return sum(map(get_arrangements_2, get_records(filepath)))
@@ -81,4 +107,5 @@ if __name__ == '__main__':
     # assert(hot_springs('test_01.txt') == )
     # assert(hot_springs('puzzle_input.txt') == )
 
-    print(hot_springs('puzzle_input.txt'))
+    print(hot_springs('test_01.txt'))
+    # print(hot_springs('puzzle_input.txt'))
