aa4e4c850f9c18de95c368c488b43d3db210d11f
ionelberdin <ionelberdin@gmail.com>
2023-12-22 19:52:58 +0100

    Test data for part 1 passing, but not converging for puzzle input

--- 2023/22_sand_slabs/sand_slabs.py
+++ 2023/22_sand_slabs/sand_slabs.py
@@ -1,131 +1,128 @@
 ''' Advent of Code 2023
-    Day 20: Pulse Propagation
-    https://adventofcode.com/2023/day/20
+    Day 22: Sand Slabs
+    https://adventofcode.com/2023/day/22
 
 Status:
     - Part 1:
-        * Test 1: TBD
+        * Test 1: PASS
         * Puzzle: TBD
     - Part 2:
         * Test 2: TBD
         * Puzzle: TBD
 '''
 
-import queue
-import re
+from functools import reduce
 
-from itertools import pairwise
+# 0. Code common to both parts of the problem
+class Brick(object):
 
-# 0. Functions that are common to both problems
+    collection = set()
+    fallen = 0
 
-class Module(object):
-    def __init__(self, name:str, destination:list, state:bool=False):
-        self.name = name
-        self.destination = destination
-        self.state = state
-        self.pulse = False
-        print(self.name, self.destination, self.state, self.pulse)
+    def __init__(self, brick_init, brick_end):
+        x0, y0, z0 = brick_init
+        x1, y1, z1 = brick_end
+        self.x = tuple(sorted([x0, x1]))
+        self.y = tuple(sorted([y0, y1]))
+        self.z = tuple(sorted([z0, z1]))
 
-    def process_pulse(self, pulse:bool, *args):
-        self.pulse = pulse
-        self.send_pulses()
+        self.is_grounded = False
+        self.supported_by = set()
 
-    def send_pulses(self):
-        print(self.destination)
-        for receiver_name in self.destination:
-            print(receiver_name)
-            PulsePropagator.queue.put((self.name, self.pulse, receiver_name)) 
+        Brick.add(self)
 
-class Broadcaster(Module):
-    pass
+    def can_fall(self):
+        if (self.z[0] == 1):
+            self.supported_by.add('ground')
+            self.is_grounded = True
+            return False
 
-class FlipFlop(Module):
-    def process_pulse(self, pulse:bool, *args):
-        if (pulse):
-            # high pulse --> ignore
-            return
-        # otherwise, flip between on and off
-        self.state = not self.state
-        # If it was off, it switches on and sends a high pulse.
-        # If it was on, it turns off and sends a low pulse.
-        self.pulse = self.state
+        if (self.is_grounded):
+            return False
 
-        self.send_pulses()
+        other_bricks = filter(lambda brick: brick is not self, Brick.collection)
+        bricks_directly_below = filter(lambda brick: brick.is_directly_below_of(self), other_bricks)
+        
+        if (len(bricks_directly_below := list(bricks_directly_below)) > 0):
+            for brick in bricks_directly_below:
+                self.supported_by.add(brick)
+                if (brick.is_grounded):
+                    self.is_grounded = True
+            return False
 
-class Conjunction(Module):
-    def add_connected_sender(self, sender_name:str):
-        try:
-            self.memory[sender_name] = False
-        except:
-            self.memory = {sender_name: False}
+        self.z = (self.z[0] - 1, self.z[-1] - 1)
+        Brick.fallen += 1
+        return True
 
-    def process_pulse(self, pulse:bool, sender_name:str, *args):
-        # First it updates its memory for that input.
-        self.memory[sender_name] = pulse
+    def is_directly_below_of(self, brick):
+        ''' Checks if self is directly below brick '''
+        if (brick.z[0] != self.z[-1] + 1):
+            return False
 
-        # If it remembers high pulses (True) for all inputs (senders in memory),
-        # it sends a low pulse (False); otherwise, it sends a high pulse (True).
-        self.pulse = not(all(self.memory.values()))
+        x_overlap = ((brick.x[0] <= self.x[0] <= brick.x[-1]) or
+                     (brick.x[0] <= self.x[-1] <= brick.x[-1]) or
+                     ((brick.x[0] > self.x[0]) and (self.x[-1] > brick.x[-1])))
 
-        self.send_pulses()
+        y_overlap = ((brick.y[0] <= self.y[0] <= brick.y[-1]) or
+                     (brick.y[0] <= self.y[-1] <= brick.y[-1]) or
+                     ((brick.y[0] > self.y[0]) and (self.y[-1] > brick.y[-1])))
 
-class PulsePropagator(object):
+        if (x_overlap and y_overlap):
+            return True
 
-    LINE_PATTERN = re.compile('([^\s]+) \-\> ([\w\,\s]+)')
-    TYPES = {'b': Broadcaster, '%': FlipFlop, '&': Conjunction}
+        return False
 
-    queue = queue.SimpleQueue()
+    @classmethod
+    def add(cls, brick):
+        cls.collection.add(brick)
+
+    @classmethod
+    def init(cls):
+
+        cls.collection = set()
+
+class SandSlabs(object):
 
     def __init__(self, filepath:str):
-        self.modules = {}
+        Brick.init()
         with open(filepath, 'r') as f:
             while (input_line := f.readline().strip('\n\s\t')):
                 self.process_input_line(input_line)
 
-        print(self.modules)
+    def get_disintegratable_briks(self):
+        ''' Disintegratable bricks are those that, if removed, no other bricks would fall '''
+        bricks_with_only_one_support = filter(lambda brick: len(brick.supported_by) == 1, Brick.collection)
+        essential_bricks = map(lambda brick: brick.supported_by, bricks_with_only_one_support)
+        essential_bricks = set(reduce(lambda x, y: x | y, essential_bricks))
 
-        # Connect Conjunction inputs
-        conjunctions = filter(lambda m: type(m) == 'Conjunction', self.modules)
-        for conjunction in conjunctions:
-            module_name = conjunction.name
-            senders = filter(lambda m: module_name in m.destination, self.modules)
-            for sender in senders:
-                conjunction.add_connected_sender(sender.name)
+        return Brick.collection - essential_bricks
 
-        # Create the button
-        button = Module('button', ['broadcaster',])
-        button.send_pulses()
-
-        while (pulse_tuple := self.queue.get()):
-            print(pulse_tuple)
-            sender_name, pulse, module_name = pulse_tuple
-            print(sender_name, pulse, module_name)
-            self.modules[module_name].process_pulse(pulse, sender_name)
-
+    def let_bricks_fall(self):
+        bricks = sorted(Brick.collection, key=lambda brick: brick.z[0])
+        while (any(map(lambda brick: brick.can_fall(), filter(lambda x: not x.is_grounded, bricks)))):
+            print
+            continue
 
     def process_input_line(self, input_line:str):
-        parse_input = self.LINE_PATTERN.search(input_line)
-        module_name = parse_input.group(1)
-        module_type = module_name[0]
-        if (module_type != 'b'):
-            module_name = module_name[1:]
-        destination = parse_input.group(2).split(", ")
-        self.modules[module_name] = self.TYPES[module_type](module_name, destination)
+        brick_init, brick_end = input_line.split('~')
+        brick_init = tuple([int(x) for x in brick_init.split(',')])
+        brick_end = tuple([int(x) for x in brick_end.split(',')])
+        Brick(brick_init, brick_end)
+
 
 # 1. Functions that are specific for problem 1
 
-def solve_part_1(filepath:str, with_hex:bool=False):
-    pulse_propagator = PulsePropagator(filepath)
-    return 1  # TODO: map correct output
+def solve_part_1(filepath:str):
+    sand_slabs = SandSlabs(filepath)
+    print("Number of bricks:", len(Brick.collection))
+    sand_slabs.let_bricks_fall()
+    print("Bricks fallen:", Brick.fallen)
+    return len(sand_slabs.get_disintegratable_briks())
 
 # 2. Functions that are specific for problem 2
 
-def solve_part_2(filepath:str, with_hex:bool=False):
-    pulse_propagator = PulsePropagator(filepath)
-    return 1  # TODO: map correct output
-
 if __name__ == '__main__':
-    # assert(solve_part_1('test_01.txt') == 62)
+    assert(solve_part_1('test_01.txt') == 5)
     # assert(pipe_maze_1('puzzle_input.txt') == 6733)
 
-    print(solve_part_1('test_01.txt'))
+    print(solve_part_1('puzzle_input.txt'))
