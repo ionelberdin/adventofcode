74953ee27828682fd3b78c4452f699a2936796f6
ionelberdin <ionelberdin@gmail.com>
2023-12-23 12:42:39 +0100

    Solution to part 1 passing

--- 2023/23_long_walk/solver.py
+++ 2023/23_long_walk/solver.py
@@ -1,54 +1,176 @@
 ''' Advent of Code 2023
-    Day 00: Skeleton Script
-    https://adventofcode.com/2023/day/00
+    Day {00}: {Title of the Day}
+    https://adventofcode.com/2023/day/{00}
+    Keywords: {List of keywords}
 
 Status:
     - Part 1:
-        * Test 1: TBD
-        * Puzzle: TBD
+        * Test 1: PASS
+        * Puzzle: PASS
     - Part 2:
         * Test 2: TBD
         * Puzzle: TBD
+
+Coordinate system (x, y) chosen as:
+    - x: row starting from 0 at the top of the input data.
+         Range = (0, lines)
+    - y: column starting from 0 at the left of the input data.
+         Range = (0, line lenght)
 '''
 
+from queue import SimpleQueue
+from timeit import default_timer
 
-# 0. Code common to both parts of the problem
+class Tile(object):
 
-class Main(object):
+    collection = {}
+
+    def __init__(self, x:int, y:int, tile_type:str):
+        self.x = x
+        self.y = y
+        self.type = tile_type
+        self.neighbours = set()
+        Tile.add(self)
+
+    @classmethod
+    def add(cls, tile):
+        cls.collection[(tile.x, tile.y)] = tile
+
+    @classmethod
+    def get(cls, x:int, y:int):
+        try:
+            return cls.collection[(x, y)]
+        except:
+            return None
+
+    @classmethod
+    def find_neighbours(cls, tile=None):
+        if (tile is None):
+            for tile in cls.collection.values():
+                cls.find_neighbours(tile)
+            return
+
+        x = tile.x
+        y = tile.y
+        if (tile.type in '.v'):
+            neighbour = cls.get(x + 1, y)
+            if neighbour is not None:
+                tile.neighbours.add(neighbour)
+        if (tile.type in '.^'):
+            neighbour = cls.get(x - 1, y)
+            if neighbour is not None:
+                tile.neighbours.add(neighbour)
+        if (tile.type in '.>'):
+            neighbour = cls.get(x, y + 1)
+            if neighbour is not None:
+                tile.neighbours.add(neighbour)
+        if (tile.type in '.<'):
+            neighbour = cls.get(x, y - 1)
+            if neighbour is not None:
+                tile.neighbours.add(neighbour)
+
+    @classmethod
+    def init(cls):
+        cls.collection = {}
+
+    def __str__(self):
+        return "({}, {}, {})".format(self.x, self.y, self.type)
+
+class Path(object):
+
+    def __init__(self, tiles, last_visited_tile):
+        self.tiles = set(tiles)
+        self.last_visited_tile = last_visited_tile
+
+    def get_next_tiles(self):
+        return self.last_visited_tile.neighbours - self.tiles
+
+    def __len__(self):
+        return len(self.tiles) - 1
+
+    def __repr__(self):
+        output = ""
+        for x, row in enumerate(self.map):
+            for y, tile_type in enumerate(row):
+                tile_type = 'x' if (tile_type == '.') else tile_type
+                tile = Tile.get(x, y)
+                output += tile_type if (tile not in self.tiles) else tile.type
+            output += '\n'
+        return output
+
+    @classmethod
+    def store_map(cls, map:str):
+        cls.map = map
+
+
+class Solver(object):
 
     def __init__(self, filepath:str):
+        Tile.init()
+        self.map = []
+        self.path = None
+        self.paths = SimpleQueue()
+        self.result = 0
         with open(filepath, 'r') as f:
+            x = 0
             while (input_line := f.readline().strip('\n\s\t')):
-                self.process_input_line(input_line)
+                self.map.append(input_line)
+                self.process_input_line(input_line, x)
+                x += 1
+        Path.store_map(self.map)
+        self.xmax = x - 1
 
-    def process_input_line(self, input_line:str):
-        pass
+    def process_input_line(self, input_line:str, x:int):
+        for y, tile_type in enumerate(input_line):
+            if (tile_type == '#'):
+                continue
+            Tile(x, y, tile_type)
+
+    def solve(self, part:int):
+        if (part == 1):
+            return self.solve_part_1()
+        elif (part == 2):
+            return self.solve_part_2()
 
     def solve_part_1(self):
-        return None
+        Tile.find_neighbours()
+
+        start = list(filter(lambda tile: tile.x == 0, Tile.collection.values()))
+        self.paths.put(Path(set(start), start[0]))
+
+        while (not self.paths.empty()):
+            path = self.paths.get()
+            next_tiles = path.get_next_tiles()
+            if (len(next_tiles) == 0):
+                if (path.last_visited_tile.x == self.xmax):
+                    if (len(path) > self.result):
+                        self.path = path
+                        self.result = len(path)
+                continue
+            for tile in next_tiles:
+                self.paths.put(Path(path.tiles | set([tile]), tile))
+        return self.result
 
     def solve_part_2(self):
         return None
 
 
-# 1. Code specific for part 1
+def solve(filepath:str, part:int):
+    print("Solving part {} with:".format(part), filepath)
+    start = default_timer()
+    solver = Solver(filepath)
+    result = solver.solve(part=part)
+    duration = default_timer() - start
+    print("Solved in {}s".format(duration))
+    print("Result:", result)
+    return result
 
-def solve_part_1(filepath:str):
-    print("Solving part 1 with:", filepath)
-    main = Main(filepath)
-    return main.solve_part_1()
-
-# 2. Code specific for part 2
-
-def solve_part_2(filepath:str):
-    print("Solving part 2 with:", filepath)
-    main = Main(filepath)
-    return main.solve_part_1()
 
 if __name__ == '__main__':
-    # assert(solve_part_1('test_01.txt') == TBD)
-    # assert(solve_part_1('puzzle_input.txt') == TBD)
-    # assert(solve_part_2('test_01.txt') == TBD)
-    # assert(solve_part_2('puzzle_input.txt') == TBD)
+    assert(solve('test_01.txt', part=1) == 94)
+    # assert(solve('puzzle_input.txt', part=1) == 2178)  # takes more than 5s
+    # assert(solve('test_01.txt', part=2) == TBD)
+    # assert(solve('puzzle_input.txt', part=2) == TBD)
 
-    print(solve_part_1('test_01.txt'))
+    print(solve('test_01.txt', part=1))
+    print(solve('puzzle_input.txt', part=1))
