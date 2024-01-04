b247ec21fba15b18f9969700a7802f76b553a3c0
ionelberdin <ionelberdin@gmail.com>
2024-01-04 13:36:52 +0100

    Numbers seem to be correctly retrieved, but their position retrieval needs to be improved

--- 2023/03_gear_ratios/solver.py
+++ 2023/03_gear_ratios/solver.py
@@ -12,18 +12,23 @@ Status:
         * Puzzle: TBD
 '''
 
+import re
+
 from timeit import default_timer
 
+INT_PATTERN = re.compile('\d+')
 
 class Solver(object):
 
     def __init__(self, filepath:str):
         with open(filepath, 'r') as f:
-            while (input_line := f.readline().strip('\n\s\t')):
-                self.process_input_line(input_line)
+            for line_number, input_line in enumerate(f.readlines()):
+                self.process_input_line(line_number, input_line.strip('\n\s\t'))
 
-    def process_input_line(self, input_line:str):
-        pass
+    def process_input_line(self, line_number:int, input_line:str):
+        numbers = INT_PATTERN.findall(input_line)
+        for number in numbers:
+            print(line_number, number, input_line.index(number))
 
     def solve(self, part:int):
         if (part == 1):
@@ -32,6 +37,11 @@ class Solver(object):
             return self.solve_part_2()
 
     def solve_part_1(self):
+        """ Strategy:
+            1. Get the position from all numbers
+            2. Get the position of all symbols.
+            3. Filter out numbers that don't have a adjoining symbol.
+            """
         return None
 
     def solve_part_2(self):
@@ -50,7 +60,7 @@ def solve(filepath:str, part:int):
 
 
 if __name__ == '__main__':
-    # assert(solve('test_01.txt', part=1) == TBD)
+    # assert(solve('test_01.txt', part=1) == 4361)
     # assert(solve('puzzle_input.txt', part=1) == TBD)
     # assert(solve('test_01.txt', part=2) == TBD)
     # assert(solve('puzzle_input.txt', part=2) == TBD)
