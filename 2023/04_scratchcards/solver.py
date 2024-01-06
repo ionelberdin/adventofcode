8fbe506a7fe1adfe462d48b0b2407dfadb78b6d7
ionelberdin <ionelberdin@gmail.com>
2024-01-06 17:14:20 +0100

    Solution to part 1 pf day 4 passing

--- 2023/04_scratchcards/solver.py
+++ 2023/04_scratchcards/solver.py
@@ -5,25 +5,44 @@
 
 Status:
     - Part 1:
-        * Test 1: TBD
-        * Puzzle: TBD
+        * Test 1: PASS
+        * Puzzle: PASS
     - Part 2:
         * Test 2: TBD
         * Puzzle: TBD
 '''
+import re
 
 from timeit import default_timer
 
+line_pattern = 'Card\s+(?P<card_id>\d+)\:\s+'
+line_pattern += '(?P<winning_numbers>[\d\s]+)'
+line_pattern += '\s\|\s+'
+line_pattern += '(?P<your_numbers>[\d\s]+)'
+
+LINE_PATTERN = re.compile(line_pattern)
+SPLIT_PATTERN = re.compile('\s+')
 
 class Solver(object):
 
     def __init__(self, filepath:str):
+        self.filepath = filepath
+        self.points = 0
         with open(filepath, 'r') as f:
             while (input_line := f.readline().strip('\n\s\t')):
                 self.process_input_line(input_line)
 
     def process_input_line(self, input_line:str):
-        pass
+        line = LINE_PATTERN.search(input_line)
+        winning_numbers = set(re.split(SPLIT_PATTERN, line.group('winning_numbers')))
+        your_numbers = set(re.split(SPLIT_PATTERN, line.group('your_numbers')))
+
+        your_winning_numbers = len(your_numbers & winning_numbers)
+
+        if (your_winning_numbers) == 0:
+            return
+
+        self.points += 2 ** (your_winning_numbers - 1) 
 
     def solve(self, part:int):
         if (part == 1):
@@ -32,7 +51,7 @@ class Solver(object):
             return self.solve_part_2()
 
     def solve_part_1(self):
-        return None
+        return self.points
 
     def solve_part_2(self):
         return None
@@ -50,9 +69,9 @@ def solve(filepath:str, part:int):
 
 
 if __name__ == '__main__':
-    # assert(solve('test_01.txt', part=1) == TBD)
-    # assert(solve('puzzle_input.txt', part=1) == TBD)
+    assert(solve('test_01.txt', part=1) == 13)
+    assert(solve('puzzle_input.txt', part=1) == 20107)
     # assert(solve('test_01.txt', part=2) == TBD)
     # assert(solve('puzzle_input.txt', part=2) == TBD)
 
-    print(solve('test_01.txt', part=1))
+    print(solve('puzzle_input.txt', part=1))
