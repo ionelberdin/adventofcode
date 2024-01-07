f953f0d131eafb50d027babe26221a401978f496
ionelberdin <ionelberdin@gmail.com>
2024-01-07 19:13:11 +0100

    Part 2 of day 4 passing!

--- 2023/04_scratchcards/solver.py
+++ 2023/04_scratchcards/solver.py
@@ -1,15 +1,15 @@
 ''' Advent of Code 2023
     Day 4: Scratchcards
     https://adventofcode.com/2023/day/4
-    Keywords: {List of keywords}
+    Keywords: sets, recursion
 
 Status:
     - Part 1:
         * Test 1: PASS
         * Puzzle: PASS
     - Part 2:
-        * Test 2: TBD
-        * Puzzle: TBD
+        * Test 2: PASS
+        * Puzzle: PASS
 '''
 import re
 
@@ -26,23 +26,27 @@ SPLIT_PATTERN = re.compile('\s+')
 class Solver(object):
 
     def __init__(self, filepath:str):
+        self.cards = {}
         self.filepath = filepath
-        self.points = 0
+
         with open(filepath, 'r') as f:
             while (input_line := f.readline().strip('\n\s\t')):
                 self.process_input_line(input_line)
 
     def process_input_line(self, input_line:str):
         line = LINE_PATTERN.search(input_line)
+
+        card_id = int(line.group('card_id'))
         winning_numbers = set(re.split(SPLIT_PATTERN, line.group('winning_numbers')))
         your_numbers = set(re.split(SPLIT_PATTERN, line.group('your_numbers')))
 
         your_winning_numbers = len(your_numbers & winning_numbers)
 
+        self.cards[card_id] = your_winning_numbers
+
         if (your_winning_numbers) == 0:
             return
 
-        self.points += 2 ** (your_winning_numbers - 1) 
 
     def solve(self, part:int):
         if (part == 1):
@@ -51,10 +55,24 @@ class Solver(object):
             return self.solve_part_2()
 
     def solve_part_1(self):
-        return self.points
+        winning_cards = filter(lambda x: x > 0, self.cards.values())
+        points = map(lambda x: 2 ** (x - 1), winning_cards)
+        return sum(points)
 
     def solve_part_2(self):
-        return None
+        number_of_cards_won = len(self.cards)
+        
+        cards = [(key, value) for key, value in self.cards.items() if value > 0]
+
+        while (len(cards) > 0):
+            card_id, value = cards.pop()
+            number_of_cards_won += value
+            for copy_id in range(card_id + 1, card_id + value + 1):
+                value = self.cards[copy_id]
+                if (value > 0):
+                    cards.append((copy_id, value))
+
+        return number_of_cards_won
 
 
 def solve(filepath:str, part:int):
@@ -71,7 +89,7 @@ def solve(filepath:str, part:int):
 if __name__ == '__main__':
     assert(solve('test_01.txt', part=1) == 13)
     assert(solve('puzzle_input.txt', part=1) == 20107)
-    # assert(solve('test_01.txt', part=2) == TBD)
-    # assert(solve('puzzle_input.txt', part=2) == TBD)
+    assert(solve('test_01.txt', part=2) == 30)
+    assert(solve('puzzle_input.txt', part=2) == 8172507)
 
-    print(solve('puzzle_input.txt', part=1))
+    print(solve('puzzle_input.txt', part=2))
