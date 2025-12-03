6ca7c06ea7c40371033fe95d448e4f9fca5e8b3e
Ion <ionelberdin@gmail.com>
2025-12-03 17:35:31 +0100

    Part 2 prepared for development

--- 2025/03_lobby/solver.py
+++ 2025/03_lobby/solver.py
@@ -38,7 +38,6 @@ def get_joltage(number:str) -> int:
 def solve_part_1(filepath:str) -> int:
     print("Solving part 1 with:", filepath)
     start = default_timer()
-    # brute force solution
     
     result = sum(map(get_joltage, parse_file(filepath)))
 
@@ -46,14 +45,15 @@ def solve_part_1(filepath:str) -> int:
     print(f"Result of part 1: {result} ({duration}s)")
     return result
 
+def get_higher_joltage(number:str, digits:int) -> str:
+
+    return '-1'
+
 def solve_part_2(filepath:str) -> int:
     print("Solving part 2 with:", filepath)
     start = default_timer()
 
-    # brute force solution
-    result = 0
-    for ids in parse_file(filepath):
-        result += sum(map(check_invalid2, range(ids[0], ids[1]+1)))
+    result = sum(map(lambda x: int(get_higher_joltage(x, 12)), parse_file(filepath)))
 
     duration = default_timer() - start
     print(f"Result of part 2: {result} ({duration}s)")
@@ -63,7 +63,7 @@ def solve_part_2(filepath:str) -> int:
 if __name__ == '__main__':
     
     assert(solve_part_1('test.txt') == 357)
-    assert(solve_part_1('puzzle.txt') == 21898734247)
+    assert(solve_part_1('puzzle.txt') == 17113)
 
     assert(solve_part_2('test.txt') == 4174379265)
     assert(solve_part_2('puzzle.txt') == 28915664433)  # too high
