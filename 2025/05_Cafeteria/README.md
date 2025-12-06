acb8eb2fb5d26dfdae00b92c249205adbaad172e
Ion <ionelberdin@gmail.com>
2025-12-06 11:05:37 +0100

    Part 1 solved and PASSing

--- 2025/05_Cafeteria/README.md
+++ 2025/05_Cafeteria/README.md
@@ -4,3 +4,28 @@ https://adventofcode.com/2025/day/5
 ## Part 1
 Parsing first part of the input file is necessary to load the available ranges.
 The ranges can be merged when they intersect.
+
+The ranges are consolidated to have the minimum number of ranges needed.
+This is actually not necessary, but could help improving performance for long
+lists of numbers to check.
+
+Solving part 1 with: test.txt
+2 ranges considered
+Result of part 1: 3 (0.00032359990291297436s)
+Solving part 1 with: puzzle.txt
+98 ranges considered
+Result of part 1: 828 (0.005678099929355085s)
+
+Comparison with the simple solution in which ranges are not consolidated:
+Solving part 1 with: test.txt
+4 ranges considered
+Result of part 1: 3 (0.00032350001856684685s)
+Solving part 1 with: puzzle.txt
+174 ranges considered
+Result of part 1: 828 (0.00579450000077486s)
+
+Actually, both solutions are comparable in time, hence it seems in the puzzle
+case there's an equilibrium between number of ranges and test numbers in the
+input file.
+
+If there were many more test numbers, then the consolidation could help.
--- 2025/05_Cafeteria/solver.py
+++ 2025/05_Cafeteria/solver.py
@@ -4,8 +4,8 @@
 
 Status:
     - Part 1:
-        * Test: TODO
-        * Puzzle: TODO
+        * Test: PASS
+        * Puzzle: PASS
     - Part 2:
         * Test: TODO
         * Puzzle: TODO
@@ -15,11 +15,50 @@ from timeit import default_timer
 from collections.abc import Iterable
 from itertools import chain
 
+def get_containing_range(ranges:set[tuple[int,int]], value:int) -> tuple[int,int] | None:
+    for (a, b) in ranges:
+        if a<= value <= b:
+            return (a, b)
+    return None
 
-def parse_file(filepath:str) -> Iterable[str]:
-    with open(filepath, 'r') as file:
-        for line in file:
-            yield line.strip('\s\t\n')
+
+def consolidate_ranges(file) -> set[tuple[int]]:
+    # read first until first empty line
+    ranges = set()
+    
+    for line in file:
+        line = line.strip('\s\t\n')
+        if line == '':
+            break
+
+        a, b = (int(x) for x in line.split('-'))
+        ranges.add((a, b))
+        continue
+
+        # Consolidation proves to be slower for the actual puzzle of this problem
+        range_a = get_containing_range(ranges, a)
+        range_b = get_containing_range(ranges, b)
+
+        if (range_a is not None) and (range_b is not None):
+            if range_a == range_b:
+                continue
+            ranges.remove(range_a)
+            ranges.remove(range_b)
+            range_min = min((range_a[0], range_b[0]))
+            range_max = max((range_a[1], range_b[1]))
+            ranges.add((range_min, range_max))
+        
+        elif range_a is None and (range_b is not None):
+            ranges.remove(range_b)
+            ranges.add((a, range_b[1]))
+
+        elif range_a is not None and (range_b is None):
+            ranges.remove(range_a)
+            ranges.add((range_a[0], b))
+        else:
+            ranges.add((a, b))
+
+    return ranges
 
 
 
@@ -28,8 +67,15 @@ def solve_part_1(filepath:str) -> int:
     start = default_timer()
     
     file = open(filepath, 'r')
+    ranges = consolidate_ranges(file)
+    print(len(ranges))
 
-    result = -1 # TODO
+    result = 0
+    for line in file:
+        number = int(line.strip('\s\t\n'))
+        result += get_containing_range(ranges, number) is not None
+
+    file.close()
 
     duration = default_timer() - start
     print(f"Result of part 1: {result} ({duration}s)")
@@ -50,7 +96,7 @@ def solve_part_2(filepath:str) -> int:
 if __name__ == '__main__':
     
     assert(solve_part_1('test.txt') == 3)
-    assert(solve_part_1('puzzle.txt') == 1508)
+    assert(solve_part_1('puzzle.txt') == 828)
 
     assert(solve_part_2('test.txt') == 4174379265)
     assert(solve_part_2('puzzle.txt') == 28915664433) 
