004c3fd4806873627aa1a3b95914c354313857d1
Ion <ionelberdin@gmail.com>
2025-12-06 13:48:08 +0100

    Part 2 passing

--- 2025/06_trash_compactor/README.md
+++ 2025/06_trash_compactor/README.md
@@ -29,8 +29,25 @@ Result of part 1: 5877594983578 (0.002976899966597557s)
 ```
 
 ## Part 2
-TBC
+The second part requires a different type of processing, but the idea is the same.
+
+```python
+result = 0 
+numbers = []
+for i in range(len(lines[0])-1,-1,-1):
+    numbers.append('')
+    for n in range(len(lines)-1):
+        numbers[-1] += lines[n][i] if lines[n][i] != ' ' else ''
+    if numbers[-1] == '':
+        numbers.pop()
+    if lines[-1][i] in '+*':
+        result += reduce(OPERATORS[lines[-1][i]], map(int, numbers))
+        numbers = []
+```
 
 ```
-...
+Solving part 2 with: test.txt
+Result of part 2: 3263827 (0.00023799994960427284s)
+Solving part 2 with: puzzle.txt
+Result of part 2: 11159825706149 (0.004394000046886504s) 
 ```
--- 2025/06_trash_compactor/solver.py
+++ 2025/06_trash_compactor/solver.py
@@ -7,8 +7,8 @@ Status:
         * Test: PASS
         * Puzzle: PASS
     - Part 2:
-        * Test: TODO
-        * Puzzle: TODO
+        * Test: PASS
+        * Puzzle: PASS
 '''
 
 from timeit import default_timer
@@ -51,7 +51,22 @@ def solve_part_2(filepath:str) -> int:
     print("Solving part 2 with:", filepath)
     start = default_timer()
 
-    result = -1
+    lines = list()
+    with open(filepath, 'r') as file:
+        for line in file:
+            lines.append(line.strip('\n'))
+    
+    result = 0 
+    numbers = []
+    for i in range(len(lines[0])-1,-1,-1):
+        numbers.append('')
+        for n in range(len(lines)-1):
+            numbers[-1] += lines[n][i] if lines[n][i] != ' ' else ''
+        if numbers[-1] == '':
+            numbers.pop()
+        if lines[-1][i] in '+*':
+            result += reduce(OPERATORS[lines[-1][i]], map(int, numbers))
+            numbers = []
 
     duration = default_timer() - start
     print(f"Result of part 2: {result} ({duration}s)")
@@ -63,5 +78,5 @@ if __name__ == '__main__':
     assert(solve_part_1('test.txt') == 4277556)
     assert(solve_part_1('puzzle.txt') == 5877594983578)
 
-    assert(solve_part_2('test.txt') == 14)
-    assert(solve_part_2('puzzle.txt') == 354282804136607) # too high
+    assert(solve_part_2('test.txt') == 3263827)
+    assert(solve_part_2('puzzle.txt') == 11159825706149)
