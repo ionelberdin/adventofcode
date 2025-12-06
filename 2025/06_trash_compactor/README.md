9f9b6ad94597dba8ce0cb7986dabdce7f88b09b6
Ion <ionelberdin@gmail.com>
2025-12-06 13:16:32 +0100

    Part 1 passing

--- 2025/06_trash_compactor/README.md
+++ 2025/06_trash_compactor/README.md
@@ -2,10 +2,30 @@
 https://adventofcode.com/2025/day/6
 
 ## Part 1
-TBC
+Parsing needs to be done for the whole file and then apply the operators iteratively.
+
+The reduce function from `functools` comes handy for this purpose:
+
+```python
+OPERATORS = {
+    '+': lambda x, y: x+y,
+    '*': lambda x, y: x*y
+}
+
+#...file processing...
+# lines:list[list[int]] contains all numeric lines as lists of integers
+# operators:list[str] contains a list of all operators in str format
+
+result = 0 
+for i, operator in enumerate(operators):
+    result += reduce(OPERATORS[operator], [x[i] for x in lines])
+```
 
 ```
-...
+Solving part 1 with: test.txt
+Result of part 1: 4277556 (0.0002586999908089638s)
+Solving part 1 with: puzzle.txt
+Result of part 1: 5877594983578 (0.002976899966597557s)
 ```
 
 ## Part 2
--- 2025/06_trash_compactor/solver.py
+++ 2025/06_trash_compactor/solver.py
@@ -4,22 +4,43 @@
 
 Status:
     - Part 1:
-        * Test: TODO
-        * Puzzle: TODO
+        * Test: PASS
+        * Puzzle: PASS
     - Part 2:
         * Test: TODO
         * Puzzle: TODO
 '''
 
 from timeit import default_timer
-from collections.abc import Iterable
+from functools import reduce
+import re
 
+
+OPERATORS = {
+    '+': lambda x, y: x+y,
+    '*': lambda x, y: x*y
+}
+
+SEP = re.compile(r'[\s\t]+')
 
 def solve_part_1(filepath:str) -> int:
     print("Solving part 1 with:", filepath)
     start = default_timer()
     
-    result = -1
+    lines = list()
+    with open(filepath, 'r') as file:
+        for line in file:
+            try:
+                line = line.strip('\s\t\n')
+                line = list(filter(lambda x: x != '', SEP.split(line)))
+                lines.append(list(map(int, line)))
+            except:
+                operators = list(line)
+    
+    result = 0 
+    for i, operator in enumerate(operators):
+        result += reduce(OPERATORS[operator], [x[i] for x in lines])
+
 
     duration = default_timer() - start
     print(f"Result of part 1: {result} ({duration}s)")
@@ -39,8 +60,8 @@ def solve_part_2(filepath:str) -> int:
 
 if __name__ == '__main__':
     
-    assert(solve_part_1('test.txt') == 3)
-    assert(solve_part_1('puzzle.txt') == 828)
+    assert(solve_part_1('test.txt') == 4277556)
+    assert(solve_part_1('puzzle.txt') == 5877594983578)
 
     assert(solve_part_2('test.txt') == 14)
     assert(solve_part_2('puzzle.txt') == 354282804136607) # too high
